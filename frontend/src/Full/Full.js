849b2d838c3f6bf8001cfadc6aa50eea63b46ca0
unknown <201951073@iiitvadodara.ac.in>
2021-08-16 00:57:20 +0530

    error fix

--- frontend/src/Full/Full.js
+++ frontend/src/Full/Full.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -7,10 +6,12 @@ import Spinner from "../Ui/Spinner";
 import styles from "./Full.module.css";
 import image from "../Image/doctors.png";
 import Creator from "../ethereum/Creator";
+import Card from "../itemCard/itemCard";
 
 const Full = React.memo((props) => {
   const [creator, setCreator] = useState({});
   const [amount, setAmount] = useState({});
+  const [merchs, setMerchs] = useState([]);
   const [showSpinner, setshowSpinner] = useState(false);
   useEffect(async () => {
     const data = {
@@ -23,32 +24,41 @@ const Full = React.memo((props) => {
       },
     };
 
-    const res = await axios.post(
-      "http://localhost:3000/creator/creatorById",
-      data,
-      config
-    );
-    setCreator(res.data.creator);
-
-    const ctr = Creator(res.data.creator.contractAddress);
-
-    const b = await ctr.methods.bal().call();
-    console.log(b);
-  }, []);
-
-  const tipHandler = async () => {
-    const ctr = Creator(creator.contractAddress);
     try {
-      const accounts = await web3.eth.getAccounts();
       setshowSpinner(true);
-      await ctr.methods.tip().send({
-        from: accounts[0],
-        value: web3.utils.toWei(amount),
-      });
+      const meerch = await axios.post(
+        "http://localhost:3000/creator/getMerchandise",
+        data,
+        config
+      );
+      console.log(meerch.data.merchandises);
+      setMerchs(meerch.data.merchandises);
+
+      const res = await axios.post(
+        "http://localhost:3000/creator/creatorById",
+        data,
+        config
+      );
+      setCreator(res.data.creator);
+
+      const ctr = Creator(res.data.creator.contractAddress);
+
+      const b = await ctr.methods.bal().call();
+      console.log(b);
     } catch (error) {
       setshowSpinner(false);
       console.log(error);
     }
+  }, []);
+
+  const tipHandler = async () => {
+    const ctr = Creator(creator.contractAddress);
+
+    const accounts = await web3.eth.getAccounts();
+    await ctr.methods.tip().send({
+      from: accounts[0],
+      value: web3.utils.toWei(amount),
+    });
 
     const data = {
       name: creator.name,
@@ -61,101 +71,27 @@ const Full = React.memo((props) => {
         token: localStorage.getItem("token"),
       },
     };
-    try {
-      const res = await axios.post(
-        "http://localhost:3000/creator/addTransaction",
-        data,
-        config
-      );
-      console.log(res);
-      setshowSpinner(false);
-    } catch (error) {
-      setshowSpinner(false);
-      console.log(error);
-    }
+
+    const res = await axios.post(
+      "http://localhost:3000/creator/addTransaction",
+      data,
+      config
+    );
+    console.log(res);
   };
-=======
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
-import axios from 'axios'
-import web3 from '../ethereum/web3'
 
-import styles from './Full.module.css'
-import image from '../Image/doctors.png'
-import Creator from '../ethereum/Creator'
-import Card from '../itemCard/itemCard'
-
-const Full = React.memo(props => {
-
-    const [creator, setCreator] = useState({});
-    const [amount, setAmount] = useState({});
-    const [merchs, setMerchs] = useState([]);
-
-    useEffect( async () => {
-
-        const data = {
-           id: props.match.params.id
-          };
-
-          let config = {
-            headers: {
-                token: localStorage.getItem('token')
-            }
-        }
-
-       
-        const meerch = await axios.post("http://localhost:3000/creator/getMerchandise", data, config)
-        console.log(meerch.data.merchandises)
-        setMerchs(meerch.data.merchandises)
-        
-        const res = await axios.post("http://localhost:3000/creator/creatorById", data, config)
-        setCreator(res.data.creator)
-       
-            const ctr = Creator(res.data.creator.contractAddress) 
-
-            const b = await ctr.methods.bal().call();
-            console.log(b);
-        
-      }, []);
-
-      const tipHandler = async() => {
-            const ctr = Creator(creator.contractAddress)
-
-            const accounts = await web3.eth.getAccounts();
-            await ctr.methods.tip().send({
-              from: accounts[0],
-              value: web3.utils.toWei(amount),
-            });
-
-            const data = {
-                name: creator.name,
-                amount: amount,
-                address: creator.contractAddress
-               };
-     
-               let config = {
-                 headers: {
-                     token: localStorage.getItem('token')
-                 }
-             }
-     
-             const res = await axios.post("http://localhost:3000/creator/addTransaction", data, config)
-             console.log(res);
-      }
-
-      let items = (
-        <div>
-          {merchs?.map((merch) => (
-           <Card
-           title={merch.title}
-           desc={merch.description}
-           price={merch.price}
-           image={merch.image}/>
-          ))}
-        </div>
-      );
-
->>>>>>> c431532f93f18f4fd3962812f62497891f7cbdbc
+  let items = (
+    <div>
+      {merchs?.map((merch) => (
+        <Card
+          title={merch.title}
+          desc={merch.description}
+          price={merch.price}
+          image={merch.image}
+        />
+      ))}
+    </div>
+  );
 
   return (
     <div>
@@ -164,7 +100,6 @@ const Full = React.memo(props => {
       <div>Name</div>
       <div>{creator.name}</div>
 
-<<<<<<< HEAD
       <div>description</div>
       <div>
         Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -187,26 +122,9 @@ const Full = React.memo(props => {
       />
 
       <button onClick={tipHandler}>Tip</button>
+
+      {items}
     </div>
-=======
-       <div>description</div>
-       <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-
-       <div>Email</div>
-       <div>{creator.email}</div>
-
-       <div>Account address</div>
-       <div>{creator.contractAddress}</div>
-
-        <input 
-        placeholder="how much you want to tip" 
-        onChange={event => setAmount(event.target.value)} />
-       
-       <button onClick={tipHandler}>Tip</button>
-
-       {items}
-   </div>
->>>>>>> c431532f93f18f4fd3962812f62497891f7cbdbc
   );
 });
 
