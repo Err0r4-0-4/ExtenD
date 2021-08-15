23cb99e9077e5acdf419fd2dfa12b1525e902d61
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-08-15 15:15:29 +0530

    Responsive

--- frontend/src/CreatorProfile/CreatorProfile.js
+++ frontend/src/CreatorProfile/CreatorProfile.js
@@ -1,96 +1,101 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import styles from './CreatorProfile.module.css'
-import image from '../Image/creator.png'
+import styles from "./CreatorProfile.module.css";
+import image from "../Image/creator.png";
 
 const Creators = React.memo(() => {
-
-
   const [creator, setCreator] = useState([]);
 
-  useEffect( async () => {
-
+  useEffect(async () => {
     const data = {
-       id: localStorage.getItem('id')
-      };
+      id: localStorage.getItem("id"),
+    };
 
-      let config = {
-        headers: {
-            token: localStorage.getItem('token')
-        }
-    }
+    let config = {
+      headers: {
+        token: localStorage.getItem("token"),
+      },
+    };
 
-    let res = await axios.post("http://localhost:3000/creator/creatorById", data, config);
+    let res = await axios.post(
+      "http://localhost:3000/creator/creatorById",
+      data,
+      config
+    );
     // console.log(res.data);
     setCreator(res.data.creator);
 
-    res = await axios.post("http://localhost:3000/creator/getContracts",data, config);
+    res = await axios.post(
+      "http://localhost:3000/creator/getContracts",
+      data,
+      config
+    );
     console.log(res);
-    console.log("res")
+    console.log("res");
 
-   
-        // const ctr = Creator(res.data.creator.contractAddress) 
+    // const ctr = Creator(res.data.creator.contractAddress)
 
-        // const b = await ctr.methods.bal().call();
-        // console.log(b);
-    
+    // const b = await ctr.methods.bal().call();
+    // console.log(b);
   }, []);
 
   const onUploadHandler = (event) => {
-
     // this.setState({loading:true})
 
     const formData = new FormData();
 
-    formData.append(
-      "file",
-      event.target.files[0]
-    );
+    formData.append("file", event.target.files[0]);
 
-    formData.append(
-        "title", "aaaaaaaaaaa"
-    )
+    formData.append("title", "aaaaaaaaaaa");
 
-    formData.append(
-        "description", "bbbbbbbb"
-    )
-    
+    formData.append("description", "bbbbbbbb");
+
     let config = {
-        headers: {
-            token: localStorage.getItem("token"),
-        }
-      }
+      headers: {
+        token: localStorage.getItem("token"),
+      },
+    };
 
-      axios.post('http://localhost:3000/creator/uploadContract', formData, config)
-        .then( response =>{
-            // this.setState({loading:false})
-            console.log(response)
-        }).catch( e => {
-            // this.setState({loading:false})
-            console.log(e)
-        })
-      
-  }
+    axios
+      .post("http://localhost:3000/creator/uploadContract", formData, config)
+      .then((response) => {
+        // this.setState({loading:false})
+        console.log(response);
+      })
+      .catch((e) => {
+        // this.setState({loading:false})
+        console.log(e);
+      });
+  };
 
   return (
     <div>
+      <img src={image} className={styles.image}></img>
 
-    <img src={image} className={styles.image}></img>
+      <div>Name</div>
+      <div>{creator.name}</div>
 
-     <div>Name</div>
-       <div>{creator.name}</div>
+      <div>description</div>
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </div>
 
-       <div>description</div>
-       <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+      <div>Email</div>
+      <div>{creator.email}</div>
+      <div>ETh recieved</div>
+      <div>1</div>
 
-       <div>Email</div>
-       <div>{creator.email}</div>
+      <button>Transfer</button>
+      <div>Account address</div>
+      <div>{creator.contractAddress}</div>
 
-       <div>Account address</div>
-       <div>{creator.contractAddress}</div>
-
-       <input type="file" onChange={onUploadHandler}/>
+      <input type="file" onChange={onUploadHandler} />
     </div>
   );
 });
