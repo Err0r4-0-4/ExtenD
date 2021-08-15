2dcec7f4a36899f8c5c35f3b3399e0c45adc9087
sidharth <201952234@iiitvadodara.ac.in>
2021-08-15 13:34:01 +0530

    creator profile added

--- Backend/Middleware/isAuth.js
+++ Backend/Middleware/isAuth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.header("token");
+  console.log(token + "token")
   if (!token) {
     return res.status(401).send("Unauthorized!");
   }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from "./Pages/Login";
 import Creators from "./Creators/Creators";
 import CreatorHome from "./Pages/CreatorHome";
 import Full from "./Full/Full";
+import CreatorProfile from './CreatorProfile/CreatorProfile'
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" exact component={Login} />
           <Route path="/home" exact component={CreatorHome} />
           <Route path="/creators" exact component={Creators} />
-
+          <Route path="/creatorProfile" exact component={CreatorProfile} />
           <Route path="/:id" exact component={Full} />
           {/* <Route path='/creator' exact component={Home}/> */}
         </Switch>
