0802a89b7d3b76f4c635ccf688e8046c37bbdb18
unknown <201951073@iiitvadodara.ac.in>
2021-08-16 10:45:58 +0530

    aaa

--- frontend/src/Creator/Creator.js
+++ frontend/src/Creator/Creator.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Slide from "react-reveal/Slide";
+//import Slide from "react-reveal/Slide";
 import {
   GrFacebookOption,
   GrInstagram,
@@ -13,55 +13,53 @@ import styles from "./Creator.module.css";
 import Card from "../Ui/Card";
 const Creators = React.memo((props) => {
   return (
-    <Slide up>
-      <Card>
-        {props.image ? (
-          <img
-            id="base64image"
-            src={`data:image/jpeg;base64,${props.image}`}
-            className={styles.image}
-          />
-        ) : (
-          <img src={image} className={styles.image} />
-        )}
+    <Card>
+      {props.image ? (
+        <img
+          id="base64image"
+          src={`data:image/jpeg;base64,${props.image}`}
+          className={styles.image}
+        />
+      ) : (
+        <img src={image} className={styles.image} />
+      )}
 
-        <div className={styles.text}>
-          <div className={styles.location}>
-            <GrLocation /> India
-          </div>
-          <div className={styles.name}>{props.name}</div>
-
-          <div className={styles.dis}>Description</div>
-
-          <ul className={styles.social}>
-            <li className={styles.face}>
-              <a href="#">
-                <GrFacebookOption />
-              </a>
-            </li>
-            <li className={styles.insta}>
-              <a href="#">
-                <GrInstagram />
-              </a>
-            </li>
-
-            <li className={styles.twitter}>
-              <a href="#">
-                <GrTwitter />
-              </a>
-            </li>
-            <li className={styles.snap}>
-              <a href="#">
-                <GrSnapchat />
-              </a>
-            </li>
-          </ul>
-          <div className={styles.link}>
-            <Link to={"/" + props.id}>View creator</Link>
-          </div>
+      <div className={styles.text}>
+        <div className={styles.location}>
+          <GrLocation /> India
         </div>
-      </Card>
-    </Slide>
+        <div className={styles.name}>{props.name}</div>
+
+        <div className={styles.dis}>Description</div>
+
+        <ul className={styles.social}>
+          <li className={styles.face}>
+            <a href="#">
+              <GrFacebookOption />
+            </a>
+          </li>
+          <li className={styles.insta}>
+            <a href="#">
+              <GrInstagram />
+            </a>
+          </li>
+
+          <li className={styles.twitter}>
+            <a href="#">
+              <GrTwitter />
+            </a>
+          </li>
+          <li className={styles.snap}>
+            <a href="#">
+              <GrSnapchat />
+            </a>
+          </li>
+        </ul>
+        <div className={styles.link}>
+          <Link to={"/" + props.id}>View creator</Link>
+        </div>
+      </div>
+    </Card>
   );
 });
 
